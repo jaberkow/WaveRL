688fcfbd552cbac55095b726f63e90cca8561143
jaberkow <jackberkowitz88@gmail.com>
2019-06-10 12:28:55 -0700

    added the rllib example environment and training script

--- complex_envs/simple_environment.py
+++ complex_envs/simple_environment.py
@@ -12,6 +12,12 @@ import pickle
 import os
 import yaml
 
+from ray.rllib.models import FullyConnectedNetwork, Model, ModelCatalog
+
+import ray
+from ray import tune
+from ray.tune import grid_search
+
 class SimpleCorridor(gym.Env):
     """Example of a custom env in which you have to walk down a corridor.
     You can configure the length of the corridor via the env config."""
@@ -35,3 +41,38 @@ class SimpleCorridor(gym.Env):
             self.cur_pos += 1
         done = self.cur_pos >= self.end_pos
         return [self.cur_pos], 1 if done else 0, done, {}
+
+class CustomModel(Model):
+    """Example of a custom model.
+    This model just delegates to the built-in fcnet.
+    """
+
+    def _build_layers_v2(self, input_dict, num_outputs, options):
+        self.obs_in = input_dict["obs"]
+        self.fcnet = FullyConnectedNetwork(input_dict, self.obs_space,
+                                           self.action_space, num_outputs,
+                                           options)
+        return self.fcnet.outputs, self.fcnet.last_layer
+
+if __name__ == "__main__":
+    # Can also register the env creator function explicitly with:
+    # register_env("corridor", lambda config: SimpleCorridor(config))
+    ray.init()
+    ModelCatalog.register_custom_model("my_model", CustomModel)
+    tune.run(
+        "PPO",
+        stop={
+            "timesteps_total": 10000,
+        },
+        config={
+            "env": SimpleCorridor,  # or "corridor" if registered above
+            "model": {
+                "custom_model": "my_model",
+            },
+            "lr": grid_search([1e-2, 1e-4, 1e-6]),  # try different lrs
+            "num_workers": 1,  # parallelism
+            "env_config": {
+                "corridor_length": 5,
+            },
+        },
+    )
