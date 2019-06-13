320699a401eace14cbd180bc04be589455b21335
jaberkow <jackberkowitz88@gmail.com>
2019-06-13 00:14:33 -0700

    first edition of general training script

--- python/envs/simple_env/simple_environment.py
+++ python/envs/simple_env/simple_environment.py
@@ -14,27 +14,20 @@ import pickle
 import os
 import yaml
 
+from ray.rllib.models import FullyConnectedNetwork, Model, ModelCatalog
+
+import ray
+from ray import tune
+from ray.tune import grid_search
+from ray.tune.registry import register_env
+
 class SimpleLineViewer:
-    """
-    This class is for creating simple plots of a one dimensional time series
-    """
     def __init__(self):
         fig = plt.figure()
         plt.show(block=False)
-    def render(self,trajectory,save=False):
-        """
-        This method is the actual render operation of the simple corridor class
-
-        Inputs:
-            trajectory:  Array of shape (num_timesteps,), the agent's trajectory over time
-            save: Boolean, whether or not to save the trajectory to a file
-        """
+    def render(self,trajectory):
         plt.plot(range(len(trajectory)),trajectory)
         plt.pause(0.001)
-        if save:
-            plt.savefig('test')
-    def close(self):
-        plt.close()
 
 
 
@@ -43,12 +36,6 @@ class SimpleCorridor(gym.Env):
     You can configure the length of the corridor via the env config."""
 
     def __init__(self, config):
-        """
-        Constructor for the simple corridor OpenAI gym
-
-        Inputs:
-            config:  A dict, must contain a 'corridor_length' entry
-        """
         self.end_pos = config["corridor_length"]
         self.cur_pos = 0
         self.action_space = Discrete(2)
@@ -74,12 +61,46 @@ class SimpleCorridor(gym.Env):
         self.trajectory.append(self.cur_pos)
         return [self.cur_pos], 1 if done else 0, done, {}
 
-    def render(self,save=False,mode='human'):
+    def render(self,mode='human'):
         if self.viewer == None:
             #Build the viewing object
             self.viewer=SimpleLineViewer()
-        self.viewer.render(self.trajectory,save=save)
-    def close(self):
-        if self.viewer != None:
-            self.viewer.close()
-            self.viewer = None
+            self.viewer.render(self.trajectory)
+
+
+class CustomModel(Model):
+    """Example of a custom model.
+    This model just delegates to the built-in fcnet.
+    """
+
+    def _build_layers_v2(self, input_dict, num_outputs, options):
+        self.obs_in = input_dict["obs"]
+        self.fcnet = FullyConnectedNetwork(input_dict, self.obs_space,
+                                           self.action_space, num_outputs,
+                                           options)
+        return self.fcnet.outputs, self.fcnet.last_layer
+
+if __name__ == "__main__":
+    # Can also register the env creator function explicitly with:
+    register_env("corridor", lambda config: SimpleCorridor(config))
+    ray.init()
+    ModelCatalog.register_custom_model("my_model", CustomModel)
+    tune.run(
+        "PPO",
+        stop={
+            "timesteps_total": 10000,
+        },
+        config={
+            "env": SimpleCorridor,  # or "corridor" if registered above
+            "model": {
+                "custom_model": "my_model",
+            },
+            "lr": grid_search([1e-2, 1e-4, 1e-6]),  # try different lrs
+            "num_workers": 1,  # parallelism
+            "env_config": {
+                "corridor_length": 5,
+            },
+        },
+        checkpoint_freq=50,
+        checkpoint_at_end=True
+    )
