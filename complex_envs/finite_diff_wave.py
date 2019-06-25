c4d4d42cfb80d3383277a57cf7e97b3529a457c1
jaberkow <jackberkowitz88@gmail.com>
2019-06-25 14:27:03 -0700

    large refactor to the finite difference simulator

--- complex_envs/finite_diff_wave.py
+++ complex_envs/finite_diff_wave.py
@@ -25,35 +25,28 @@ class Wave1D:
         #scale the force width by system length
         self.force_width *= self.L
 
-        #driving force profile is a superposition of waves
-        self.Nwaves = config['num_waves'] 
-        self.drive_period = config['drive_period']
-        self.drive_frequency = 2.0*np.pi/self.drive_period
-        self.drive_magnitude = config['drive_magnitude']
-
         #the lattice spacing
         self.dx = float(self.L)/float(self.Nx)
 
+        #Mesh points in space
+        self.x_mesh = np.linspace(0.0,self.L,self.Nx+1)
+        
         #the courant number
         self.C = self.c_speed *self.dt/self.dx
         self.C2 = self.C**2 #helper number
 
-        #Mesh points in space
-        self.x_mesh = np.linspace(0.0,self.L,self.Nx+1)
-
         #recalibrate the resolutions to account for rounding
         self.dx = self.x_mesh[1] - self.x_mesh[0]
         
+        #We set up the conditions of the system before warmup period
 
         #The system is always initially at rest
-        #TODO: generalize this by making V passable parameter
         self.V = lambda x: 0
         
         #we assume the system starts completely flat
+        #TODO: generalize this
         self.I = lambda x: 0
         
-        #setup the source term
-        self.source_term = lambda x,t: 0
 
         #allocate memory for the recursive solution arrays
         self.u     = np.zeros(self.Nx + 1)   # Solution array at new time level
@@ -72,10 +65,11 @@ class Wave1D:
         #we reset the time and step index
         self.t = 0
         self.n = 0
-        #We reset the driving force
-        self.setup_driving_force()
-        #set up a source term that is just the driving force for now
-        self.source_term = lambda x,t : self.driving_force(x,t)
+        
+        #we set the force vals to zero
+        self.force_vals = np.zeros(self.num_force_points)
+        
+        
         #we set the initial condition of the solution 1 time level back
         for i in range(0,self.Nx+1):
             self.u_n[i]=self.I(self.x_mesh[i])
@@ -85,45 +79,13 @@ class Wave1D:
         for i in range(1,self.Nx):
             self.u[i] =self.u_n[i] + self.dt*self.V(self.x_mesh[i])
             self.u[i]+=0.5*self.C2*(self.u_n[i-1] - 2*self.u_n[i] + self.u_n[i+1])
-            self.u[i]+=0.5*(self.dt**2)*self.source_term(self.x_mesh[i],self.t)
+            self.u[i]+=0.5*(self.dt**2)*self.impulse_term(self.x_mesh[i])
         #force boundary conditions
         self.u[0]=0
         self.u[self.Nx]=0
         #switch solution steps
         self.u_nm1[:] = self.u_n
         self.u_n[:] = self.u
-    
-    def setup_driving_force(self):
-        """
-        Creates the external driving force that is oscillating the bridge
-        
-        It has a spatial profile that is a sum of harmonics of the system,
-        and an oscillating temporal profile.
-        """
-        frequency_list = []
-        for i in range(self.Nwaves):
-            wave_num = np.random.randint(low=-(self.Nx-1), high=self.Nx+1 )
-            frequency = float(wave_num)*np.pi/float(self.L)
-            frequency_list.append(frequency)
-        self.spatial_freq_array = np.array(frequency_list)
-        spatial_array = np.sum(np.sin(self.x_mesh[:,np.newaxis]*self.spatial_freq_array[np.newaxis,:]),axis=1)
-        spatial_magnitude = np.max(np.abs(spatial_array))
-        self.epsilon = 1.0/float(spatial_magnitude)
-
-        
-    def driving_force(self,x,t):
-        """
-        The driving force function.
-        
-        Inputs:
-            x - a scalar, position in the domain
-            t - a scalar, current timestep
-        """
-
-
-        force_profile = np.sin(self.drive_frequency*t)*np.sum(np.sin(x*self.spatial_freq_array))
-
-        return force_profile/self.epsilon
     
     def single_step(self):
         """
@@ -135,7 +97,7 @@ class Wave1D:
         for i in range(1,self.Nx):
             self.u[i] = -self.u_nm1[i] + 2*self.u_n[i] 
             self.u[i] += self.C2*(self.u_n[i-1] - 2*self.u_n[i] + self.u_n[i+1]) 
-            self.u[i] += self.dt**2*self.source_term(self.x_mesh[i],self.t)
+            self.u[i] += (self.dt**2)*self.impulse_term(self.x_mesh[i])
         #force boundary conditions
         self.u[0] = 0  
         self.u[self.Nx] = 0
@@ -143,7 +105,16 @@ class Wave1D:
         #switch solution steps
         self.u_nm1[:] = self.u_n
         self.u_n[:] = self.u
-    def impulse_term(self,x,force_vals):
+    
+    def take_in_action(self,action):
+        """
+        This method acts as the interface where the agent applies an action to environment.
+        For this simulator, it's simply a setter method for the force_vals attribute that
+        determine the profile of the impulse term.
+        """
+        self.force_vals = np.copy(action)
+    
+    def impulse_term(self,x):
         """
         The function definition for the active damping terms
         
@@ -152,18 +123,12 @@ class Wave1D:
             force_vals - A vector of shape (self.num_force_points),
                 the (signed) values of the force at each piston point
         """
-        return np.sum(force_vals*np.exp(-0.5* ((x-self.force_locations)**2 )/self.force_width))
-    def take_in_action(self,action):
+        return np.sum(self.force_vals*np.exp(-0.5* ((x-self.force_locations)**2 )/self.force_width))
+    
+    def get_impulse_profile(self):
         """
-        This function takes in the action from the agent and translates it into a change
-        into the wave system's source term.
-        
-        
-        """
-        self.source_term = lambda x,t : self.driving_force(x,t) + self.impulse_term(x,action)
-    def get_impulse_profile(self,force_vals):
-        """
-        A utility function for returning an array representing the shape of the resulting impulse force
+        A utility function for returning an array representing the shape of the resulting impulse 
+        force, this is used for rendering the history of actions taken by the agent.
         
         Inputs:
             force_vals - A vector of shape (self.num_force_points),
@@ -171,8 +136,9 @@ class Wave1D:
         """
         profile = []
         for i in range(self.Nx+1):
-            profile.append(self.impulse_term(self.x_mesh[i],force_vals))
+            profile.append(self.impulse_term(self.x_mesh[i]))
         return np.array(profile)
+    
     def get_observation(self):
         """
         This is an interface that returns the observation of the system, which is modeled
@@ -189,13 +155,22 @@ class Wave1D:
         observation[0,:,1]=self.u_n
         observation[0,:,2]=self.u_nm1
         return observation
-    def get_loss(self):
+
+    def energy(self):
         """
-        Returns the integrated squared magnitude of the system's deviations from zero
-        
-        Outputs:
-            squared_deviation - The integrated value of the squared deviation over the domain,
-                uses simpson's rule
+        Computes the internal energy of the system based upon the integral functional for
+        the 1-D wave equation.
+
+        There will be inherent periodicity even in the absence of external driving force,
+        due to the finite difference approximation.
+
+        See http://web.math.ucsb.edu/~grigoryan/124A/lecs/lec7.pdf for details
         """
+
+        dudt = (self.u-self.u_n)/self.dt #time derivative
+        dudx = np.gradient(self.u,self.x_mesh) #space derivative
         
-        return simps(self.u**2,self.x_mesh)
+        space_term = -self.u*np.gradient(dudx,self.x_mesh) #alternative tension energy
+        energy_density = dudt**2 + (self.c_speed**2)*(dudx**2)
+        #energy_density = dudt**2 + (self.c_speed**2)*space_term
+        return 0.5*simps(energy_density,self.x_mesh)
